a23b1528927d5e457683cab60f6eadcbeac8ee46
hwanee <hwanee@LAPTOP-8BMVT3NO>
2018-05-31 00:21:33 +0900

    .

---
+++ src/main/java/com/dh/common/interceptor/LoginInterceptor.java
@@ -0,0 +1,43 @@
+package com.dh.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.dh.app.HomeController;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter{
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		System.out.println("session check===================");
+		//요청의 세션이 존재하지않으면 세션생성 X
+		HttpSession session = request.getSession(false);
+		
+		if(session == null) {
+			System.out.println("세션없음.");
+			response.sendRedirect(request.getContextPath()+"/home/loginForm.do");
+			return false;
+		}
+		
+		
+		if(session.getAttribute("member") == null) {
+			System.out.println("member세션없음");
+			response.sendRedirect(request.getContextPath()+"/home/loginForm.do");
+			return false;
+		}
+		
+		
+		return true;
+	}
+	
+
+}
